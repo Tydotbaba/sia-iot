ac11c8b00578a8344ebc78ab992ac817de9f1aa3
Tydotbaba <oyes2011@gmail.com>
2019-11-27 04:50:05 +0100

    added files

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,39 @@ const router = vertex.router()
 
 //this is the vairable to check the state of Power in the home
 let state = false;
+let queryTime = 0; 
+// function startCountDown(seconds){
+// 	var counter = seconds;
+// 	var interval = setInterval(() => {
+// 		console.log(counter);
+// 		counter--;
+// 		if (counter < 0 ) {
+			
+// 			// code here will run when the counter reaches zero.
+			
+// 			clearInterval(interval);
+// 			console.log('Ding!');
+// 		}	
+// 	}, 1000);
+// }
+
+
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
 	information, view here: https://mustache.github.io/#demo */
 router.get('/', (req, res) => {
+	// if the state is true, start the coutdown timer
+	if(state === true){
+		let now = new Date().getTime();
+		console.log(now)
+		let seconds = now - queryTime
+		seconds = Math.floor((seconds % (1000 * 60)) / 1000);
+		if(seconds > 10) {
+			//set state to false
+			state = false;
+		}
+	}
 	//res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 	if(state === true)	
 		{
@@ -21,9 +49,12 @@ router.get('/', (req, res) => {
 
 /*  This route render the status of electricity in the user's home */
 router.get('/user', (req, res) => {
+	state = true;
+	 // Get today's date and time
+  	queryTime = new Date().getTime();
+  	console.log(queryTime);
 	res.json({
 		confirmation: 'success',
-		app: process.env.TURBO_APP_ID
 	})
 })
 
